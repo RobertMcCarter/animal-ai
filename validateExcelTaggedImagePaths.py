384041dbbb33e0ba616b4fa478230ac8488426ec
RobertMcCarter <RobertMcCarter@Outlook.com>
2021-03-23 22:05:09 -0400

    Another Excel file helper utility

---
+++ validateExcelTaggedImagePaths.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import os
+
+from typing import List
+
+# First - load all the folders
+excelFileToValidate = r"D:\data\NRSI\NRSI_1033H_Camera Data_2019_03_14_All Data.xlsx"
+
+# Load the Excel file with the file paths to validate
+df = pd.read_excel(excelFileToValidate, "Wildlife Camera Data_QAQC")
+df = df.replace(np.nan, '', regex=True)
+
+# columns = list(df)
+# print(columns)
+
+count = 0
+taggedImagePaths: List[str] = []
+for i, row in df.iterrows():
+    count += 1
+    # Guess the folder from the Excel file
+    taggedImagePath:str = str(row["Picture File"]).strip()
+    if os.path.isfile(taggedImagePath):
+        print(taggedImagePath)
+        taggedImagePaths.append(taggedImagePath)
+    # else:
+    #     print(f'Failed to find tagged image from row: {i} - "{taggedImagePath}"')
+
+
+numTaggedImages = len(taggedImagePaths)
+numMissingImages = count- numTaggedImages
+print(f"Found a total of {numTaggedImages} tagged images - out of {count} (missing {numMissingImages})")
