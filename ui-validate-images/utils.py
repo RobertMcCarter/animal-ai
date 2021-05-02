03499dc5150c8684bd4b3b03fca3574a8f0c2432
RobertMcCarter <RobertMcCarter@Outlook.com>
2021-05-01 23:38:27 -0400

    Converting the .csv file into .json

--- ui-validate-images/utils.py
+++ ui-validate-images/utils.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from typing import List, Union
 
 def readAnimalsCsvFile( path:str ) -> List[List[Union[bool,str]]]:
@@ -9,13 +10,13 @@ def readAnimalsCsvFile( path:str ) -> List[List[Union[bool,str]]]:
         # Create a csv reader object
         csvReader = csv.reader(csvfile)
 
-        # extracting field names through first row
+        # Extracting field names through first row
         next(csvReader)
 
-        # extracting each data row one by one
+        # Extracting each data row one by one
         row: List[Union[bool, str]]
         for row in csvReader:
-            col1 = True if row[0] == "True" else False
+            col1 = True if row[0] in ("True", "TRUE") else False
             col2 = row[1].strip()
             rows.append( [col1, col2] )
     return rows
@@ -28,3 +29,20 @@ def writeAnimalsCsvFile( path:str, data:List[List[Union[bool,str]]] ) -> None:
         csvWriter = csv.writer(csvfile, quotechar='|', quoting=csv.QUOTE_MINIMAL)
         for row in data:
             csvWriter.writerow(row)
+
+
+def saveCsvAsJson( data: List[List[Union[bool,str]]], outputPath:str ):
+    # First, read in the given path as a .csv file
+    jsonData = {
+        "maxViewed": 0,
+        "images": [ { "tagged": row[0], "filePath": row[1] } for row in data ]
+    }
+
+    # Write the output file
+    with open(outputPath, "w") as json_file:
+        json.dump(jsonData, json_file, indent=2)
+
+
+if __name__ == "__main__":
+    csvData = readAnimalsCsvFile(r"./animals.final.csv")
+    saveCsvAsJson(csvData, r"./animals.json")
